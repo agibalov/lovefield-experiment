94766dbc29057f1f00d5c29ab1e40321f4dbd0ff
loki2302 <loki2302@loki2302.me>
2015-12-27 19:12:45 +0300

    Now using Babel

--- karma.conf.js
+++ karma.conf.js
@@ -3,16 +3,26 @@ module.exports = function(config) {
     basePath: './',
 
     files: [
+      'node_modules/babel-polyfill/dist/polyfill.min.js',
       'bower_components/lovefield/dist/lovefield.min.js',
       'tests/**/*.spec.js'
     ],
+    preprocessors: {
+      'tests/**/*.spec.js': ['babel']
+    },
+    babelPreprocessor: {
+      options: {
+        presets: ['es2015', 'stage-0']
+      }
+    },
 
     autoWatch: true,
     frameworks: ['jasmine'],
     browsers: ['Chrome'],
     plugins: [
       'karma-chrome-launcher',
-      'karma-jasmine'
+      'karma-jasmine',
+      'karma-babel-preprocessor'
     ]
   });
 };
--- tests/dummy.spec.js
+++ tests/dummy.spec.js
@@ -1,33 +1,27 @@
-describe('lovefield', function() {
-  it('should work', function(done) {
-    var schemaBuilder = lf.schema.create('todo', 1);
+describe('lovefield', () => {
+  it('should work #2', async (done) => {
+    var schemaBuilder = lf.schema.create('todo', 1)
     schemaBuilder.createTable('Item')
       .addColumn('id', lf.Type.INTEGER)
       .addColumn('description', lf.Type.STRING)
-      .addPrimaryKey(['id']);
+      .addPrimaryKey(['id'])
 
-    var todoDb;
-    var itemTable;
-    schemaBuilder.connect().then(function(db) {
-      todoDb = db;
-      itemTable = db.getSchema().table('Item');
-      var row1 = itemTable.createRow({
-        id: 1,
-        description: 'hello there'
-      });
-      var row2 = itemTable.createRow({
-        id: 2,
-        description: 'omg'
-      });
+    var db = await schemaBuilder.connect()
+    var itemTable = db.getSchema().table('Item')
+    var row1 = itemTable.createRow({
+      id: 1,
+      description: 'hello there'
+    })
+    var row2 = itemTable.createRow({
+      id: 2,
+      description: 'omg'
+    })
 
-      return db.insertOrReplace().into(itemTable).values([row1, row2]).exec();
-    }).then(function() {
-      return todoDb.select().from(itemTable).exec();
-    }).then(function(rows) {
-      rows.forEach(function(row) {
-        console.log(row);
-      });
-      expect(rows.length).toBe(2);
-    }).then(done, done);
-  });
-});
+    await db.insertOrReplace().into(itemTable).values([row1, row2]).exec()
+    var rows = await db.select().from(itemTable).exec()
+    rows.forEach(console.log)
+    expect(rows.length).toBe(2)
+
+    done()
+  })
+})
